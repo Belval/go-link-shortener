8bd40fea37bcb790ebf0ae936e824558ddcf36dd
Edouard Belval <edouard@belval.org>
2018-04-18 16:59:35 +0000

    Add proper connection management

--- go-link-shortener/service.go
+++ go-link-shortener/service.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"database/sql"
+	"fmt"
+        "log"
+        "database/sql"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -10,14 +12,18 @@ import (
 )
 
 func linkHandler(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Path[1 : config.URLLength+1]
+        url := r.URL.Path[1 : config.URLLength+1]
 	database, _ := sql.Open("sqlite3", config.DatabasePath)
 	rows, _ := database.Query("SELECT original_url FROM urls WHERE shortened_url = ? LIMIT 1", url)
 	var originalURL string
 	rows.Next()
 	if err := rows.Scan(&originalURL); err != nil {
+                defer rows.Close()
+                defer database.Close()
 		http.Error(w, "", 404)
 	} else {
+                defer rows.Close()
+                defer database.Close()
 		http.Redirect(w, r, originalURL, 301)
 	}
 }
@@ -32,12 +38,16 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 	linkUID := uid.String()[0:config.URLLength]
 	database, _ := sql.Open("sqlite3", config.DatabasePath)
 	statement, _ := database.Prepare("INSERT INTO urls (original_url, shortened_url) VALUES (?, ?)")
-	statement.Exec(url, linkUID)
+	if _, err := statement.Exec(url, linkUID); err != nil {
+               fmt.Println(err)
+        }
+        defer statement.Close()
+        defer database.Close()
 	w.Write([]byte(config.Domain + linkUID))
 }
 
 func startWebServer() {
-	http.HandleFunc("/", linkHandler)
+        http.HandleFunc("/", linkHandler)
 	http.HandleFunc("/api/v1/new/", apiHandler)
-	http.ListenAndServe(":"+config.Port, nil)
+	log.Fatal(http.ListenAndServe(":"+config.Port, nil))
 }
